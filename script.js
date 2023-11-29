8da0219ca29c95a9b462d3b96c8976306e04bc5d
yaraano <qwertystar24@gmail.com>
2023-11-30 00:34:12 +0600

    first-commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// const MyFunction = (s) => {
+//     let left = 0
+//     let right = 0
+//
+//     for (let i = 0 ; i < s.length ; i++) {
+//         if (s[i] === '{') {
+//             left++
+//         } else {
+//             right++
+//         }
+//     }
+//     if (right === left) {
+//         return '{}'
+//     } else if (left > right) {
+//         return '{'
+//     } else {
+//         return '}'
+//     }
+// }
+//
+//
+// console.log(MyFunction('{}{}]{}{}{}}}}}}{{{{}}}}}}{{{{{{{'))
